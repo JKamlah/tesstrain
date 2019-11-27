c92270283405b2787ced4dbe0dd54de4670e4b08
JKamlah <>
2019-11-27 20:06:08 +0100

    Rework load_settings and start to implement guideline evaluation.

--- analyse.py
+++ analyse.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from functools import reduce
 from pprint import pprint
 import unicodedata
+import re
 from PIL import Image
 
 # Command line arguments.
@@ -17,6 +18,7 @@ arg_parser.add_argument("-n", "--dry-run", help="show which files would be norma
 arg_parser.add_argument("-v", "--verbose", help="show ignored files", action="store_true")
 arg_parser.add_argument("-f", "--form", help="normalization form (default: NFC)", choices=["NFC", "NFKC", "NFD", "NFKD"], default="NFC")
 arg_parser.add_argument("-c", "--categorize", help="Customized unicodedata categories", type=str, default=["Fraktur"], nargs='*')
+arg_parser.add_argument("-g", "--guidelines", help="Evaluated the dataset against some guidelines", type=str, default="OCRD-1", choices=["OCRD-1","OCRD-2","OCRD-3"])
 
 
 args = arg_parser.parse_args()
@@ -26,59 +28,63 @@ def get_defaultdict(infodictstage, newstage, instance=dict):
     infodictstage[newstage] = defaultdict(instance) if not infodictstage.get(newstage, None) else infodictstage[newstage]
 
 
-def load_catdata():
-    import re
-    catdata = defaultdict(dict)
-    cat,subcat = None, None
-    with open(Path("./unicode.categories"),"r") as fin:
+def load_settings(filename):
+    settings = defaultdict(dict)
+    setting,subsetting = None, None
+    with open(Path(filename),"r") as fin:
         for line in fin.readlines():
-            line = line.strip()
+            line = line.strip().strip(",")
             if len(line) < 1 or line[0] == "#": continue
             if line[0]+line[-1] == "[]":
-                cat = line.strip("[]")
-                get_defaultdict(catdata,cat, instance=list)
+                setting = line.strip("[]")
+                get_defaultdict(settings,setting, instance=list)
                 continue
-            if cat:
+            if setting:
                 if "=" in line:
-                    subcat, line = re.sub('\s', '', line).split('=')
-                    #catdata[cat][subcat] = []
-                if subcat and isinstance(catdata[cat][subcat],list):
+                    subsetting, line = re.sub('\s', '', line).split('=')
+                    #settings[setting][subsetting] = []
+                if subsetting and isinstance(settings[setting][subsetting],list):
                     for values in line.split(','):
                         if '-' in values and len(values.split('-')) == 2:
-                            catdata[cat][subcat].append(range(*sorted([int(val) if not "0x" in val else int(val,16) for val in values.split("-")])))
+                            settings[setting][subsetting].append(range(*sorted([int(val) if not "0x" in val else int(val,16) for val in values.split("-")])))
                         elif '-' not in values:
                             if values.isdigit():
-                                catdata[cat][subcat].append(int(values))
+                                settings[setting][subsetting].append(int(values))
                             elif values[:2] == "0x":
-                                catdata[cat][subcat].append(int(values,16))
+                                settings[setting][subsetting].append(int(values,16))
                             elif len(values) > 1:
-                                catdata[cat][subcat].append(values)
+                                settings[setting][subsetting].append(values)
                             else:
-                                catdata[cat][subcat].append(ord(values))
-        if cat and subcat:
-            return catdata
+                                settings[setting][subsetting].append(ord(values))
+        if setting and subsetting:
+            return settings
 
 
 def categorize(infodict:defaultdict,category="unicode"):
     if category == "unicode":
-        for key, val in infodict["overall"]["character"].items():
-            uname = unicodedata.name(key)
-            ucat = unicodedata.category(key)
+        for glyphe, count in infodict["overall"]["character"].items():
+            uname = unicodedata.name(glyphe)
+            ucat = unicodedata.category(glyphe)
             usubcat = uname.split(' ')[0]
             get_defaultdict(infodict["overall"], ucat[0])
             get_defaultdict(infodict["overall"][ucat[0]], usubcat)
             get_defaultdict(infodict["overall"][ucat[0]][usubcat], ucat[1])
-            infodict["overall"][ucat[0]][usubcat][ucat[1]].update({key: val})
+            infodict["overall"][ucat[0]][usubcat][ucat[1]].update({glyphe: count})
     else:
-        catdata = load_catdata()
-        if catdata and category in catdata.keys():
-            for key, val in infodict['overall']['character'].items():
-                for subcat, subdata in catdata[category].items():
-                    for subdat in subdata:
-                        if ord(key) == subdat or subdat in unicodedata.name(key).replace(" ",""):
+        categories = load_settings("./analyse-settings/categories")
+        if categories and category in categories.keys():
+            for glyphe, count in infodict['overall']['character'].items():
+                for subcat, subkeys in categories[category].items():
+                    for subkey in subkeys:
+                        if ord(glyphe) == subkey or subkey in unicodedata.name(glyphe).replace(" ",""):
                             get_defaultdict(infodict,category)
                             get_defaultdict(infodict[category],subcat)
-                            infodict[category][subcat][key] = val
+                            infodict[category][subcat][glyphe] = count
+    return
+
+
+def guidelines(text, infodict, gdlname):
+    gdl = load_settings("./analyse-settings/guidelines")
     return
 
 # Set filenames or Path
@@ -98,10 +104,21 @@ for filename in args.filename:
                 print(filename.name + " (ignored)")
             continue
 
+# Analyse the overall statistics
 get_defaultdict(infodict, "overall")
 infodict["overall"]["character"] = reduce(lambda x, y: x+y, infodict["single"].values())
+
+# Analyse the overall statistics with category statistics
 categorize(infodict,category="unicode")
+
+# Analyse the overall statistics with customized categories
 for category in args.categorize:
     categorize(infodict,category = category)
+
+# Analyse the text versus the guidelines
+guidelines(text, infodict, args.guidelines)
+
+# Print the information
 pprint(infodict["overall"],indent=4)
 pprint(infodict["Fraktur"])
+print(ord("A"))
